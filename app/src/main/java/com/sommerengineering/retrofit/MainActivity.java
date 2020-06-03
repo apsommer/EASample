8278c06cc0446dd9fe8e0f68922519746492c30d
Drew Sommer <drew@sommerengineering.com>
2020-06-03 15:46:14 -0700

    feat: Setup the Retrofit client

--- app/src/main/java/com/sommerengineering/retrofit/MainActivity.java
+++ app/src/main/java/com/sommerengineering/retrofit/MainActivity.java
@@ -4,11 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity implements Callback<List<Change>> {
+
+    static final String BASE_URL = "https://git.eclipse.org/r/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        GerritApi gerritApi = retrofit.create(GerritApi.class);
+
+        Call<List<Change>> call = gerritApi.loadChanges("status:open");
+        call.enqueue(this);
+    }
+
+    @Override
+    public void onResponse(Call<List<Change>> call, Response<List<Change>> response) {
+        
+    }
+
+    @Override
+    public void onFailure(Call<List<Change>> call, Throwable t) {
+
     }
 }
