9e339d9c3b1b5e5d2b2c9f019d2495a2f1f28463
Drew Sommer <drew@sommerengineering.com>
2020-06-03 15:32:52 -0700

    feat: Construct the data model

---
+++ app/src/main/java/com/sommerengineering/retrofit/Change.java
@@ -0,0 +1,17 @@
+package com.sommerengineering.retrofit;
+
+/**
+ * Simple POJO that represents a change in a subject text
+ */
+public class Change {
+
+    private String subject;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
