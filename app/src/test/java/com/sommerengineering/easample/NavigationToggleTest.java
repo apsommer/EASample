ad9bde535914ef6458662ca9811a83912f8248fd
Drew Sommer <drew@sommerengineering.com>
2020-06-30 22:24:52 -0700

    version: Add dependencies for JUnit suite

---
+++ app/src/test/java/com/sommerengineering/easample/NavigationToggleTest.java
@@ -0,0 +1,13 @@
+package com.sommerengineering.easample;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+@SmallTest
+public class NavigationToggleTest {
+
+
+}
